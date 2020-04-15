a81cd07be994dcbe09c5eb279f3981c8ce5cc5ba
ikuanyshbekov <ikuanyshbekov@gmail.com>
2020-04-15 12:35:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 
 	out, err := yaml.Marshal(mapResult)
 	// write the whole body at once
-	err = ioutil.WriteFile("app-out.yaml", out, 0644)
+	err = ioutil.WriteFile("app.yaml", out, 0644)
 	if err != nil {
 		panic(err)
 	}
